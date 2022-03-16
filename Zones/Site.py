9c08cccc7a6ad7aad4ede5ad9cf3e8620fbd68e8
SigveSjo <sigvesjo@gmail.com>
2022-03-16 13:03:15 +0100

    added

--- Zones/Site.py
+++ Zones/Site.py
@@ -1,20 +1,54 @@
 from Zones.Zone import Zone
+import requests
+
+
+URL = "http://127.0.0.1:3030/bot"
 
 
 class Site(Zone):
 
-    def create(self, height, width, length):
-        self.type = "Site"
-        self.height = height
-        self.width = width
-        self.length = length
-        self.containsZone = list
+    def create(self, args):
+        self.type = args[0]           
+        self.height = args[1]         
+        self.width = args[2]          
+        self.length = args[3]
+        self.id = Site.getID()         
+        self.hasBuildings = args[4]   #List
 
-    def addToKB():
-        pass
+    def addToKB(args):
+         # INPUT order_list: [type, length, width, height, hasStoreys[]]
+        # hasStoryes is a list containing ids for the storeys inside this building. 
+ 	 	# return 1 (true) when added
+        site_id = Site.getID(args)
+
+        try:
+            UPDATE = ('''
+            PREFIX bot:<https://w3id.org/bot#>
+            INSERT {
+                bot:''' + str(site_id) + ''' a bot:Building.
+                bot:''' + str(site_id) + ''' bot:hasLength "''' + str(args[1]) + '''".
+                bot:''' + str(site_id) + ''' bot:hasWidth "''' + str(args[2]) + '''".
+                bot:''' + str(site_id) + ''' bot:hasHeight "''' + str(args[3]) + '''".
+            '''
+            )   
+            for i in range(len(args[-1])):
+                UPDATE += ('''
+                bot:''' + str(args) + ''' bot:hasBuildings "''' + str(args[-1][i]) + '''".
+                    ''')
+            UPDATE += ('''}
+            WHERE {
+            }
+            ''')
+            print(UPDATE)
+            PARAMS = {"update": UPDATE}
+            r = requests.post(url = URL+"/update", data = PARAMS) 
+    
+            return 1
+        except:
+            return 0
 
     def remove():
         pass
 
-    def getID():
+    def getID(self):
         pass
