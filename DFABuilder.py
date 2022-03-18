b109c379c713513ecee6dd791ce6ffa0fa5ba67b
SigveSjo <sigvesjo@gmail.com>
2022-03-18 14:04:08 +0100

    DFAs

--- DFABuilder.py
+++ DFABuilder.py
@@ -1,4 +1,5 @@
 #import requests
+from IDGenerator import IDGenerator
 
 
 
@@ -6,14 +7,14 @@ class DFABuilder():
 
     
     def makeDFA(type, height, width, length):
-
+        id = IDGenerator.create_ID(type)
         #Read current temp file
         f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/" + type +".dfa", "r")
         txt = f.read()
         txt_replaced = txt.replace("<HEIGHT>", str(height))
         txt_replaced = txt_replaced.replace("<WIDTH>", str(width))
         txt_replaced = txt_replaced.replace("<LENGTH>", str(length))
-        txt_replaced = txt_replaced.replace("<ID>", "order_13") #TODO automize the ID part
+        txt_replaced = txt_replaced.replace("(child) block1", str(id)) #TODO automize the ID part
         f.close()
 
         #Make new DFA file
@@ -36,8 +37,23 @@ class DFABuilder():
         #else:
         #    print("Please Upload again !")
 
+    def makeDesignTemplate(): #Should take some form of ID for the order, def makeDesignTemplate(key):
+        #Design
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/design.dfa", "r")
+        txt = f.read()
+        txt = txt.replace("<ID>", "order_13") #TODO replace order 13 with automated id.
+        f.close()
+        #Design
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/tempo_design.dfa", "w")
+        f.write(txt)
+        f.close
+        #Design
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/Products/" + "order_13" + ".dfa", "a")
+        #TODO switch "building_12" with IDGenerator
+        f.write(txt)
+        f.close
+
     def generateDFA(type, height, width, length):
-        
 
         if type == "building":
             DFABuilder.makeDFA("building", height, width, length)
@@ -58,4 +74,7 @@ class DFABuilder():
     def extractDFAfile(path):
         pass
 
-DFABuilder.generateDFA("site", 0.2, 300, 500)
+
+
+DFABuilder.generateDFA("site", 0.2, 300, 500, id)
+DFABuilder.generateDFA("building", 200, 150, 300, id)
