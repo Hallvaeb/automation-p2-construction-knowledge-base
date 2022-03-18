7e800a1ef8e3f3c8f622f3bedff3194eec45cd15
SigveSjo <sigvesjo@gmail.com>
2022-03-18 12:51:29 +0100

    dfas

--- DFABuilder.py
+++ DFABuilder.py
@@ -4,26 +4,24 @@
 
 class DFABuilder():
 
-
-    
     
     def makeDFA(type, height, width, length):
 
         #Read current temp file
-        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/" + str(type) ".dfa", "r")
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/" + type +".dfa", "r")
         txt = f.read()
         txt_replaced = txt.replace("<HEIGHT>", str(height))
         txt_replaced = txt_replaced.replace("<WIDTH>", str(width))
         txt_replaced = txt_replaced.replace("<LENGTH>", str(length))
-        txt_replaced = txt_replaced.replace("<ID>", str(type) +"_12") #TODO automize the ID part
+        txt_replaced = txt_replaced.replace("<ID>", "order_13") #TODO automize the ID part
         f.close()
 
         #Make new DFA file
-        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/tempo_" + str(type) + ".dfa", "w")
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/tempo_" + type + ".dfa", "w")
         f.write(txt_replaced)
         f.close
 
-        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/Products/" + str(type) +"_12" + ".dfa", "a")
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/Products/" + "order_13" + ".dfa", "a")
         #TODO switch "building_12" with IDGenerator
         f.write(txt_replaced)
         f.close
@@ -37,27 +35,27 @@ class DFABuilder():
         #    print(uploaded.text)
         #else:
         #    print("Please Upload again !")
-    
-    def generateDFA(type, height, width, length):
-        match type:
-            case 1:
-                type = "building"
-                return DFABuilder.makeDFA("building", height, width, length)
 
-            case 2:
-                type = "site"
-                return DFABuilder.makeDFA("site", height, width, length)
+    def generateDFA(type, height, width, length):
+        
+
+        if type == "building":
+            DFABuilder.makeDFA("building", height, width, length)
+
+        elif type == "site":
+            DFABuilder.makeDFA("site", height, width, length)
             
-            case 3:
-                type = "site"
-                return DFABuilder.makeDFA("site", height, width, length)
+        elif type == "space":
+            DFABuilder.makeDFA("space", height, width, length)
             
-            case 4:
-                type = "site"
-                return DFABuilder.makeDFA("site", height, width, length)
+        elif type == "storey":
+            DFABuilder.makeDFA("storey", height, width, length)
             
     
+
 
 
     def extractDFAfile(path):
         pass
+
+DFABuilder.generateDFA("site", 0.2, 300, 500)
