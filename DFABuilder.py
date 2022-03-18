b79f5fdd1572568f267ae477a8e214b0e9a5a8cb
SigveSjo <sigvesjo@gmail.com>
2022-03-18 11:09:04 +0100

    restructure of DFA-folder

--- DFABuilder.py
+++ DFABuilder.py
@@ -1,4 +1,4 @@
-import requests
+#import requests
 
 
 
@@ -10,31 +10,33 @@ class DFABuilder():
     def makeDFA(height, width, length):
         
         #Read current temp file
-        f = open("C:\Users\sigve\OneDrive\Dokumenter\NTNU\V2022\Automatisering prosjekt\kbe-a2\DFAs\building.dfa", "r")
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/building.dfa", "r")
         txt = f.read()
-        txt_replaced = txt.replace("<HEIGTH>", str(height))
+        txt_replaced = txt.replace("<HEIGHT>", str(height))
         txt_replaced = txt_replaced.replace("<WIDTH>", str(width))
         txt_replaced = txt_replaced.replace("<LENGTH>", str(length))
+        f.close()
 
         #Make new DFA file
-        f = open("FILE" + ".dfa", "w")
+        f = open("C:/Users/sigve/OneDrive/Dokumenter/NTNU/V2022/Automatisering prosjekt/kbe-a2/DFAs/building.dfa", "w")
         f.write(txt_replaced)
         f.close
 
-        f = open("FILE" + ".dfa", "a")
-        f.write(txt_replaced)
-        f.close
+        #f = open("FILE" + ".dfa", "a")
+        #f.write(txt_replaced)
+        #f.close
 
 
         #Upload DFA file to URL
-        f = open("FILE", "r")
-        uploaded = requests.post("URL", files = {"form_field_name": f})
-        if uploaded.ok:
-            print("File uploaded successfully !")
-            print(uploaded.text)
-        else:
-            print("Please Upload again !")
+        #f = open("FILE", "r")
+        #uploaded = requests.post("URL", files = {"form_field_name": f})
+        #if uploaded.ok:
+        #   print("File uploaded successfully !")
+        #    print(uploaded.text)
+        #else:
+        #    print("Please Upload again !")
     
+
 
     def extractDFAfile(path):
         pass
